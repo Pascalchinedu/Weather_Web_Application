fb20b9926345f9d6d2bd5f432c71d0051feaa135
Pascalchinedu <pascalchindeu2000@gmail.com>
2023-03-15 00:32:17 -1200

    Update Script

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ setInterval(() => {
     const hour = time.getHours();
     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
     const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM'
+    const ampm = hour >=12 ? 'AM' : 'PM'
 
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
 
